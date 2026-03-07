59fe13e5d729b8527234576f6aa4f5ee2c239f8f
Ashikur Rahman <ashikurrahman3060@gamil.com>
2026-03-08 03:27:28 +0600

    modal implementation

--- scripts/index.js
+++ scripts/index.js
@@ -51,7 +51,78 @@ function showIssuesLength(length) {
 
 }
 
+const loadIssueDetails = async (id) => {
+    const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
 
+    const res = await fetch(url);
+    const data = await res.json();
+    // console.log(data.data);
+
+    displayIssueDetails(data.data);
+
+
+
+}
+
+const displayIssueDetails = (issue) => {
+    const modalDetailsContainer = document.getElementById("modal-details-container");
+    modalDetailsContainer.innerHTML = "";
+    const div = document.createElement("div");
+    div.innerHTML = `<div class="flex flex-col gap-4 ">
+            <h1 class="font-bold text-xl">
+                    ${issue.title}
+            </h1>
+            <div class="text-sm font-semibold flex items-center gap-2 text-gray-500">
+                <p class="px-2 py-1 rounded-full text-xs ${issue.status === "open" ? 'bg-green-100 text-green-700 border border-green-500' : 'bg-purple-100 text-purple-700 border border-purple-500'}">${issue.status === "open" ? 'Open' : 'Closed'}</p>
+                <span class="w-1 h-1 bg-gray-500 rounded-full"></span>
+
+                <p>Opened by ${issue.author}</p>
+                <span class="w-1 h-1 bg-gray-500 rounded-full"></span>
+                <p>${issue.createdAt}</p>
+            </div>
+
+            <div class="flex gap-2">
+                 ${issue.labels.includes("bug") ? `
+                <div class="p-2 rounded-full  bg-red-200 text-red-500 text-sm flex items-center">
+                        <i class="fa-solid fa-bug"></i>
+                        <p class="ml-1">bug</p>
+                    </div>`: ""}
+
+                ${issue.labels.includes("help wanted") ? `
+                <div class="p-2 rounded-full bg-yellow-200 text-yellow-600 text-sm flex items-center">
+                        <i class="fa-solid fa-hand-holding-medical"></i>
+                        <p class="ml-1">help wanted</p>
+                    </div>`: ""}
+
+                ${issue.labels.includes("documentation") ? `
+                <div class="p-2 rounded-full bg-gray-200 text-gray-600 text-sm flex items-center">
+                        <i class="fa-solid fa-book"></i>
+                        <p class="ml-1">documentation</p>
+                    </div>`: ""}
+                
+                ${issue.labels.includes("enhancement") ? `
+                <div class="p-2 rounded-full bg-green-200 text-green-600 text-sm flex items-center">
+                        <i class="fa-solid fa-wand-magic-sparkles"></i>
+                        <p class="ml-1">enhancement</p>
+                    </div>`: ""}
+                    
+                </div>
+
+                <p class="text-gray-400 text-sm">${issue.description}</p>
+                <div class="grid grid-cols-2 bg-gray-100 rounded-lg p-2">
+                    <div>
+                        <p class="text-gray-500">Assigned:</p>
+                        <p class="font-bold">${issue.assignee || "Unassigned"}</p>
+                    </div>
+                    <div>
+                        <p class="text-gray-500">Priority:</p>
+                        <h2 class="w-20 py-1 rounded-full text-red-600 bg-red-200 text-center text-sm">${issue.priority?.toUpperCase() || "LOW"}</h2>
+                    </div>
+                </div>
+        </div>`
+        modalDetailsContainer.appendChild(div);
+    document.getElementById("my_modal_5").showModal();
+}
 
 
 function showAllIssues(issues) {
@@ -64,7 +135,7 @@ function showAllIssues(issues) {
         const div = document.createElement("div");
         div.innerHTML = `
             <div
-                class="issues-card p-4 flex flex-col gap-4 h-full bg-white rounded-md border-1 border-t-4 ${issue.status === "open" ? 'border-green-500' : 'border-purple-500'} shadow-lg">
+                onclick="loadIssueDetails(${issue.id})" class="issues-card p-4 flex flex-col gap-4 h-full bg-white rounded-md border-1 border-t-4 ${issue.status === "open" ? 'border-green-500' : 'border-purple-500'} shadow-lg">
                 <div class="flex justify-between">
                     <div>
                     ${issue.status === "open" ? '<img src="./B13-A5-Github-Issue-Tracker-main/B13-A5-Github-Issue-Tracker-main/assets/Open-Status.png" alt="Open">' : '<img src="./B13-A5-Github-Issue-Tracker-main/B13-A5-Github-Issue-Tracker-main/assets/Closed-Status .png" alt="Closed">'}
